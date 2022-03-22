0a7ea7006280a6d174a0349cc8d648bb200443c9
Indra Sudirman <indrabsudirman@gmail.com>
2022-03-22 20:46:20 +0700

    Add string and map dart file

---
+++ bin/map.dart
@@ -0,0 +1,7 @@
+void main() {
+  Map<String, String> map1 = {};
+
+  var map2 = Map<String, String>();
+
+  var map3 = <String, String>{};
+}
---
+++ bin/string.dart
@@ -0,0 +1,23 @@
+void main() {
+  String firstName = 'Indra';
+  var lastName = 'Bayu';
+  
+  var fullName = '$firstName ${lastName}';
+  
+  print(fullName);
+
+  var text = 'This is \'dart\' is cool';
+  print(text);
+
+  var name = 'Indra' ' Bayu' ' Sudirman';
+  print(name);
+
+  var longText = '''Ini adalah text yang sangat
+panjang sekali. Ini adalah text yang sangat
+panjang sekali. Ini adalah text yang sangat
+panjang sekali. Ini adalah text yang sangat
+panjang sekali. Ini adalah text yang sangat
+panjang sekali''';
+
+  print(longText);
+}
