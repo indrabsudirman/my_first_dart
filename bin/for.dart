2e8a0d1f570fb678c9c826480853a3b5afbac7b2
Indra Sudirman <indrabsudirman@gmail.com>
2022-03-23 20:58:07 +0700

    finished null_safety, Add for and while

---
+++ bin/for.dart
@@ -0,0 +1,8 @@
+void main() {
+  // var count = 1;
+  for (var count = 1; count <= 10; count++) {
+    print('Perulangan ke-$count');
+
+    
+  }
+}
--- bin/null_safety.dart
+++ bin/null_safety.dart
@@ -1,4 +1,3 @@
-
 void main() {
   int? age = null;
   age = 10;
@@ -13,4 +12,15 @@ void main() {
   guest = 'Bob';
   String guestName = guest ?? 'Guest'; //?? Default value
   print(guestName);
+
+  int? numableNumber;
+  int nonNumerableNumber = numableNumber ?? 0;
+
+  print(nonNumerableNumber);
+
+  int? dataInt;
+  double? dataDouble = dataInt?.toDouble();
+
+  print(dataDouble);
+
 }
