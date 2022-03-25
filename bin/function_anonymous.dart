63d881c0a4268763ef8ffded3039fca75ab35888
indrabsudirman <indrabsudirman@gmail.com>
2022-03-25 16:01:12 +0700

    Update anonymous function

--- bin/function_anonymous.dart
+++ bin/function_anonymous.dart
@@ -2,19 +2,24 @@ void sayHello(String name, String Function(String) filter) {
   print('Hello ${filter(name)}');
 }
 
+int doMathOperator(int number1, int number2, Function operator) {
+  return operator(number1, number2);
+}
+
 void main() {
-  sayHello('John', (name) {
-    return name.toUpperCase();
-  });
+  // sayHello('John', (name) {
+  //   return name.toUpperCase();
+  // });
 
-  var upperFunction = (String name) {
-    return name.toUpperCase();
-  };
+  // var upperFunction = (String name) {
+  //   return name.toUpperCase();
+  // };
 
-  var lowerFunction = (String name) => name.toLowerCase();
+  // var lowerFunction = (String name) => name.toLowerCase();
 
-  sayHello('Indra', (name) => name.toUpperCase());
+  // sayHello('Indra', (name) => name.toUpperCase());
 
-  print(upperFunction('Dart'));
-  print(lowerFunction('Dart'));
+  // print(upperFunction('Dart'));
+  // print(lowerFunction('Dart'));
+  print(doMathOperator(2, 3, (number1, number2) => number1 * number2));
 }
